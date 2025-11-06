fa83321024e5760b1f84cc1aae9c1132f111912c
juanadiego <juanadiegolorente@gmail.com>
2025-11-06 19:46:42 +0100

    prueba

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <!--aqui iria el header-->
+    <!--aqui iria la imagen-->
+    <!--aqui iria el formulario-->
+    <form action="" method="">
+        <p>CORREO ELECTRÓNICO<input type="text"></p>
+        <p>CONTRASEÑA<input type="password"></p>
+        <button type="submit">INICIAR SESIÓN</button>
+        <a href="">Recuperar contraseña</a>
+        <a href="">¿No tienes cuenta?</a>
+        <a href="">REGÍSTRATE</a>
+    </form>
+    <!--aqui iria el footer-->
+</body>
+</html>
