b07b6d4366547cc61167bdc2d91422668ee37196
juanadiego <juanadiegolorente@gmail.com>
2026-05-09 23:33:17 +0200

    estilos regalos a falta de poner imagenes y productos mas vendidos

--- resources/views/pages/regalos.blade.php
+++ resources/views/pages/regalos.blade.php
@@ -2,14 +2,98 @@
 
 @section("content")
 
-<div class="container my-5">
-    <div class="text-center py-5">
-        <i class="bi bi-gift fs-1 text-muted"></i>
-        <h2 class="mt-3">Regalos</h2>
-        <p class="text-muted fs-5">Disponible próximamente</p>
-        <p class="text-muted">Estamos preparando esta sección para ti. ¡Vuelve pronto!</p>
-        <a href="{{ route('index') }}" class="btn btn-outline-dark mt-3">Volver al inicio</a>
+<div id="portada_regalos">
+    <div id="imagen_portada_regalos">
+        <img src="{{ asset('') }}" alt="">
     </div>
+    <div id="texto_portada_regalos">
+        <h2>Regalos para grabar</h2>
+        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget</h4>
+    </div>
+</div>
+<div id="opciones_regalos">
+    <div class="row row-cols-1 row-cols-md-3 mb-3 text-center"> 
+        <div class="col"> 
+            <div class="card mb-4 rounded-3 shadow-sm"> 
+                <img src="{{ asset('img/joyeria/anillos.png') }}" alt="anillo_regalos" class="img-fluid">
+                <h3>Compromiso y bodas</h3>
+            </div> 
+        </div> 
+        <div class="col"> 
+            <div class="card mb-4 rounded-3 shadow-sm"> 
+                 <img src="{{ asset('img/joyeria/pulseras.png') }}" alt="pulseras_regalos" class="img-fluid"> 
+                 <h3>Niños</h3>
+            </div> 
+        </div> 
+        <div class="col"> 
+            <div class="card mb-4 rounded-3 shadow-sm"> 
+                 <img src="{{ asset('img/joyeria/collares.png') }}" alt="collares_regalos" class="img-fluid"> 
+                 <h3>Cumpleaños</h3>
+            </div> 
+        </div> 
+    </div>
+</div>
+<div id="conjuntos_regalos">
+    <div id="texto_conjuntos">
+        <h2>Conjuntos</h2>
+        <h4>Encuentra conjuntos de joyas que haran tus momentos mas especiales</h4>
+    </div>
+    <div id="imagen_conjuntos">
+        <img src="{{ asset('') }}" alt="" class="img-fluid">
+    </div>
+</div><!--
+<div id="mas_vendidos_regalos">
+    <div id="titulo_mas_vendidos_regalos">
+        <h2>Más vendidos</h2>
+    </div>
+    <div id="productos_mas_vendidos_regalos">
+        <div id="producto_1">
+            <img src="" alt="">
+        </div>
+        <div id="producto_2">
+            <img src="" alt="">
+        </div>
+        <div id="producto_3">
+            <img src="" alt="">
+        </div>
+        <div id="producto_4">
+            <img src="" alt="">
+        </div>
+    </div>
+</div>-->
+<div id="imagenes_grandes_regalos">
+    <div id="imagen1">
+        <img src="{{ asset('') }}" alt="">
+    </div>
+    <div id="imagen2">
+        <img src="{{ asset('') }}" alt="">
+    </div>
+</div>
+<div id="imagenes_peques_regalos">
+    <div class="row row-cols-1 row-cols-md-3 mb-3 text-center"> 
+        <div class="col"> 
+            <div class="card mb-4 rounded-3 shadow-sm"> 
+                <img src="{{ asset('img/joyeria/anillos.png') }}" alt="anillo_regalos" class="img-fluid">
+                <h3>Menos de 50€</h3>
+            </div> 
+        </div> 
+        <div class="col"> 
+            <div class="card mb-4 rounded-3 shadow-sm"> 
+                 <img src="{{ asset('img/joyeria/pulseras.png') }}" alt="pulseras_regalos" class="img-fluid"> 
+                 <h3>Menos de 100€</h3>
+            </div> 
+        </div> 
+        <div class="col"> 
+            <div class="card mb-4 rounded-3 shadow-sm"> 
+                 <img src="{{ asset('img/joyeria/collares.png') }}" alt="collares_regalos" class="img-fluid"> 
+                 <h3>Menos de 200€</h3>
+            </div> 
+        </div> 
+    </div>
+</div>
+<div id="imagenes_grande_regalos">
+    <img src="{{ asset('') }}" alt="">
+    <h4>Menos de 500€</h4>
 </div>
 
 @endsection
