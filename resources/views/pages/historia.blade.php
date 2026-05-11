1f48655a3efed88c903da2665031625206ffc281
juanadiego <juanadiegolorente@gmail.com>
2026-05-11 21:54:38 +0200

    estilos regalos

--- resources/views/pages/historia.blade.php
+++ resources/views/pages/historia.blade.php
@@ -4,16 +4,27 @@
 
 <div class="container my-5">
     <div class="text-center py-5">
-        <h2 class="mt-3">Historia</h2>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br>
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br>
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris <br>
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in <br>
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br>
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui <br>
-            officia deserunt mollit anim id est laborum.
-        </p>
+        <h1 class="mt-3">Historia</h1>
+        <br><br>
+        <h4>
+            Fundada en el año 2012, Joyas Pérez nació del espíritu emprendedor de Wilson Pérez, <br>
+            un orfebre apasionado que decidió establecer su taller en el corazón de Soria.<br> <br>
+
+            Wilson, mucho antes de la apertura de su propia joyería, ya trabajaba como joyero, <br>
+            sin embargo, tenía una visión muy clara de lo que quería crear. <br><br>
+            Aunque previamente había trabajado y aprendido bajo las óredenes del maestro Shifu, <br>
+            quien le enseñó todo lo que hoy sabe de la orfebrería. <br><br>
+
+            Huyendo de la producción en masa y de la joyería comercial, Wilson empezó a diseñar en un pequeño local<br> 
+            combinando técnicas contemporáneas con la rica inspiración de la tierra soriana. <br><br>
+
+            Hoy en día, el proyecto de Wilson se ha consolidado como un referente de la joyería de autor en la provincia. <br>
+            Aunque sus diseños exclusivos ahora se envían a toda España gracias a su tienda online, el alma de Joyas Pérez <br>
+            sigue intacta en su coqueta boutique del centro de Soria.<br><br>
+            Allí, Wilson continúa trabajando con el mismo mimo <br>
+            que el primer día, demostrando que, con talento y dedicación, se puede crear arte vanguardista y de calidad <br>
+            desde una ciudad pequeña, sin perder nunca el trato cercano con el cliente.
+</h4>
     </div>
 </div>
 
