a75d905c3d6d3eb36cb71ce12248f198ec3dc1ea
diego <dcuencaaznar@gmail.com>
2026-05-15 21:44:18 +0200

    mas cambios

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Joyas Pérez </title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}?v={{ filemtime(public_path('favicon.ico')) }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}?v={{ filemtime(public_path('favicon.ico')) }}" type="image/x-icon">
 
     <!-- Precarga del logo para el loader -->
     <link rel="preload" href="{{ asset('images/logo.svg') }}" as="image">
